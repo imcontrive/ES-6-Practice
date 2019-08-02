f736c72ac7d3a8b4d1161d59821def32c9282b16
shubh <psubham94@gmail.com>
2019-08-02 21:22:25 +0530

    update solution

---
+++ katas/main.js
@@ -0,0 +1,7 @@
+Write a function for jumping number.
+Jumping number is the number that All adjacent digits in it differ by 1.
+
+test case: 
+jumpingNumber(9) // "jumping!!"
+jumpingNumber(57) // Not
+
